4ed55cd40225ac0763a8f8b01b98b7b90b7c30ea
brianOlaya <brilot777@gmail.com>
2020-05-06 14:29:46 -0600

    Contador realizado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state ={
+    count:0,
+    name:""
+  }
+
+  handleChange = (event)=> {
+    this.setState({name:event.target.value})
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.handleChange}  value={this.state.name}></textarea>
+        <div className="counter" >{this.state.name.length}</div>
       </div>
     );
   }
